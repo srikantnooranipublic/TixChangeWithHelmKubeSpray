751406186ffa3d8d39afaa1a26382b524968a6d2
srikant <srikns@yahoo.com>
2019-10-21 04:41:03 +0000

    selenium changes

--- tools/logSeleniumOutput.sh
+++ tools/logSeleniumOutput.sh
@@ -1,8 +1,19 @@
 SELENIUM_PODS=`kubectl get pods -n selenium |grep -v grep|grep -v NAME|awk '{print $1}'`
 
-for SELENIUM_POD in $SELENIUM_PODS;
+
+while true ;
 do
-	echo "****logs are in /opt/ca/TixChangeK8sDemo/logs/$SELENIUM_POD.Selenium.logs"
-	kubectl logs  $SELENIUM_POD -n selenium >> /opt/ca/TixChangeK8sDemo/logs/$SELENIUM_POD.Selenium.logs
+	for SELENIUM_POD in $SELENIUM_PODS;
+	do
+		echo ""
+        	echo "Looping through Selenium Pods(if multiple pods are running) - showing logs for Pod $SELENIUM_POD"
+        	sleep 5
+		kubectl logs  --tail=50  $SELENIUM_POD -n selenium 
+		#>> /opt/ca/TixChangeK8sDemo/logs/$SELENIUM_POD.Selenium.logs
+		echo ""
+        	echo "##### Done logs for Pod - $SELENIUM_POD - moving to next POD (if present) in 10 sec"
+		sleep 10
+	done
+
 done
 
